d1d2e4caf3a9cc85ad987f77081f8f0fe9a39c89
ftrrtf <facktoreal@gmail.com>
2013-06-04 15:08:48 +0300

    Disable autodetect branch

--- DependencyInjection/FtrrtfRollbarExtension.php
+++ DependencyInjection/FtrrtfRollbarExtension.php
@@ -28,18 +28,17 @@ class FtrrtfRollbarExtension extends Extension
             $loader->load('services.yml');
             $container->setParameter('ftrrtf_rollbar.access_token', $config['access_token']);
             $container->setParameter('ftrrtf_rollbar.host', $config['host']);
-            $container->setParameter('ftrrtf_rollbar.branch', $config['branch']);
             $container->setParameter('ftrrtf_rollbar.environment', $config['environment']);
             $container->setParameter('ftrrtf_rollbar.root_dir', $config['root_dir']);
             $container->setParameter('ftrrtf_rollbar.branch', $config['branch']);
-            if ($config['git_branch_auto_detect']) {
-                $gitHeadFile = $config['root_dir'] . '.git/HEAD';
-                if (file_exists($gitHeadFile)) {
-                    $parts = explode('/', file_get_contents($gitHeadFile));
-                    $detectedBranch = trim(array_pop($parts));
-                    $container->setParameter('ftrrtf_rollbar.branch', $detectedBranch);
-                }
-            }
+//            if ($config['git_branch_auto_detect']) {
+//                $gitHeadFile = $config['root_dir'] . '.git/HEAD';
+//                if (file_exists($gitHeadFile)) {
+//                    $parts = explode('/', file_get_contents($gitHeadFile));
+//                    $detectedBranch = trim(array_pop($parts));
+//                    $container->setParameter('ftrrtf_rollbar.branch', $detectedBranch);
+//                }
+//            }
         }
     }
 
