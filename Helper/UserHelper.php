d579fb492df20f69fb7cdd4f898b3aaa9c13c47a
ftrrtf <dev@ftrrtf.com>
2016-07-12 23:04:10 +0200

    No rely on __toString method of user object anymore

--- Helper/UserHelper.php
+++ Helper/UserHelper.php
@@ -2,23 +2,34 @@
 
 namespace Ftrrtf\RollbarBundle\Helper;
 
+use Symfony\Component\Security\Core\User\UserInterface;
+
 class UserHelper
 {
     /**
      * Get current user info.
      *
-     * @param mixed|object $user
+     * @param string|UserInterface $user
      *
      * @return null|array
      */
     public function buildUserData($user)
     {
-        $userData = array();
+        if (is_string($user)) {
+            return array(
+                'id' => $user,
+            );
+        }
+
+        if (!($user instanceof UserInterface)) {
+            return array();
+        }
+
         $userData['id'] = method_exists($user, 'getId')
             ? $user->getId()
-            : (string) $user;
+            : $user->getUsername();
 
-        $userData['username'] = (string) $user;
+        $userData['username'] = $user->getUsername();
 
         if (method_exists($user, 'getEmail')) {
             $userData['email'] = $user->getEmail();
